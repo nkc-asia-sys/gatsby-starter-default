98b314b062c7280b0a1597a2421a251890f01908
nkc-asia-sys <sys@nkcasia.com>
2021-03-25 17:57:17 +0900

    add env var

--- lp/card-a/src/main.js
+++ lp/card-a/src/main.js
@@ -1,1 +1,9 @@
-console.log('webpack test clear');
+const hostName = location.hostname;
+const formLink = document.getElementById('form-link');
+if (hostName.includes(process.env.D_ENV)) {
+	formLink.href = process.env.D_URL;
+} else if (hostName.includes(S_ENV)) {
+	formLink.href = process.env.S_URL;
+} else {
+	formLink.href = process.env.P_URL;
+}
