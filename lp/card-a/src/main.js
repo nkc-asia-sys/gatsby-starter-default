e43f8c3b6e6673cb8556fa46574f8e0c45750b7c
nkc-asia-sys <sys@nkcasia.com>
2021-03-25 18:44:18 +0900

    fix

--- lp/card-a/src/main.js
+++ lp/card-a/src/main.js
@@ -2,7 +2,7 @@ const hostName = location.hostname;
 const formLink = document.getElementById('form-link');
 if (hostName.includes(process.env.D_ENV)) {
 	formLink.href = process.env.D_URL;
-} else if (hostName.includes(S_ENV)) {
+} else if (hostName.includes(process.env.S_ENV)) {
 	formLink.href = process.env.S_URL;
 } else {
 	formLink.href = process.env.P_URL;
